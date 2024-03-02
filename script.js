74c9502f13f074281718bd73d913eabb6b7d86a1
Aidil.chairi <62952600+aidilchw@users.noreply.github.com>
2024-03-02 21:56:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function scrollToElement(elementSelector, instance = 0) {
+    const elements = document.querySelectorAll(elementSelector);
+    if (elements.length > instance) {
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToElement('.header');
+});
+
+link2.addEventListener('click', () => {
+    scrollToElement('.header', 1);
+});
+
+link3.addEventListener('click', () => {
+    scrollToElement('.column');
+});
+
+var tw = new Date();
+if (tw.getTimezoneOffset() == 0) (a=tw.getTime() + ( 7 *60*60*1000))
+else (a=tw.getTime());
+tw.setTime(a);
+var tahun= tw.getFullYear ();
+var hari= tw.getDay ();
+var bulan= tw.getMonth ();
+var tanggal= tw.getDate ();
+var hariarray=new Array("Minggu,","Senin,","Selasa,","Rabu,","Kamis,","Jum'at,","Sabtu,");
+var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember");
+document.getElementById("tanggalwaktu").innerHTML = hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun;
+
+var typed = new Typed((".skill"),{
+    strings : ["Junior web Developer...","Network Technician...","3D Animation...","Computer Science...","Frontend Designer..."],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
+
+var typed = new Typed((".nama"),{
+    strings : ["Aidil Chairi Willian"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
